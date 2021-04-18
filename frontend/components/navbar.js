ac308eb825f4a021ad313cf2ea359d0c3eb0eac9
Ruchikon005 <6135512005@psu.ac.th>
2021-04-18 20:52:36 +0700

    update2

--- frontend/components/navbar.js
+++ frontend/components/navbar.js
@@ -2,18 +2,21 @@ import Link from 'next/link'
 import styles from '../styles/Home.module.css'
 const Navbar = () => (
     <div className={styles.footer}>
-        <div className={styles.padding}>
-            <Link href="/"><a> Home </a></Link> |
-            <Link href="/register"><a> Register </a></Link>  |
-            <Link href="/login"><a> Login </a></Link> |
-            <Link href="/profile"><a> Profile </a></Link> |
-            <Link href="/getConfig"><a> Config </a></Link> |
-            <Link href="/logout"><a> Logout </a></Link> |
+        <div className={styles.box} id = 'text'>
+            <Link href="/" >
+                <div className = {styles.boxmanu}>Home</div>
+                </Link>
+            <Link href="/register">Register</Link>
+            <Link href="/login"><a> Login </a></Link>
+            <Link href="/profile"><a> Profile </a></Link>
+            <Link href="/getConfig"><a> Config </a></Link>
+            <Link href="/logout"><a> Logout </a></Link>
             <Link href="/foo"><a> foo </a></Link>
         </div>
 
-
     </div>
+
+
 )
 
-export default Navbar
+export default Navbar 
